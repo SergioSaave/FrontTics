84d64a13858a02fe11fefba5a2fd1bdc0b162e6d
Sergio Saavedra <sergio.saavedrajr@gmail.com>
2023-06-22 13:36:42 -0400

    Creacion grafico

--- src/gases/pages/PruebaPage.jsx
+++ src/gases/pages/PruebaPage.jsx
@@ -1,57 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+
+const GASSENSE_ENDPOINT = "http://192.168.18.50:4000/gas";
 
 export const PruebaPage = () => {
+  const [fact, setFact] = useState([]);
+
+  useEffect(() => {
+    fetch(GASSENSE_ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => {
+        setFact(data);
+      });
+  }, []);
+
+  const state = {
+    stroke: {
+      curve: 'smooth',
+    },
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91, 24],
+      },
+    ],
+  };
+
   return (
-    <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque nulla
-      minima eum ab facilis suscipit itaque pariatur qui beatae ipsa, recusandae
-      iure vitae maxime odio nesciunt cupiditate possimus? Fugiat, impedit?
-      Laudantium aliquid laborum quae debitis optio facilis, repellat libero
-      atque velit quos quas ipsam illum eius odit accusantium laboriosam, labore
-      doloribus temporibus fugit? Aliquid earum nulla delectus perspiciatis
-      iusto quam. Dolore saepe provident recusandae incidunt quam harum atque
-      explicabo asperiores nulla eveniet cupiditate voluptatibus blanditiis sed
-      neque libero, perferendis iusto impedit nesciunt, autem maxime doloremque
-      culpa fuga iure! Quia, animi? Aperiam provident non porro blanditiis
-      corporis doloremque eaque pariatur voluptatum, voluptatem illo omnis
-      labore tempora corrupti unde quam perferendis commodi? A omnis recusandae
-      voluptatum totam quibusdam perspiciatis reiciendis dicta non! Itaque
-      voluptatum at reprehenderit, exercitationem vel, eaque cumque qui ullam
-      officiis pariatur sit fugit. Animi dolor voluptates optio, cumque illum
-      velit soluta tempore ratione, vitae molestias eligendi adipisci facere
-      ipsam. Incidunt molestiae assumenda totam similique quis eveniet delectus
-      ex expedita animi praesentium cumque veniam quaerat, provident, sint
-      beatae tempore id facilis nihil est sed dolorem suscipit a error. A,
-      suscipit. Commodi odio ipsum ut corrupti exercitationem fugiat explicabo
-      eius cum omnis similique eaque dicta, rerum voluptatem earum mollitia
-      atque dolorum perferendis incidunt? Laboriosam nihil aspernatur
-      voluptatibus consequatur ipsam eaque ipsa. Atque, debitis ipsam amet,
-      neque perspiciatis tempora magni quidem modi rem voluptas nesciunt
-      reprehenderit ut similique sunt dignissimos, ducimus corrupti? Corporis
-      debitis dolor nesciunt commodi distinctio officia ipsum quidem aut. Ab
-      cupiditate perspiciatis, quibusdam corrupti eos provident ea obcaecati est
-      quasi similique laborum unde fugit dignissimos fuga dolorum, adipisci
-      laboriosam aliquid enim itaque! Praesentium, magnam sunt veritatis impedit
-      exercitationem dolorem? Neque nobis eligendi placeat! Quaerat ullam
-      exercitationem aperiam neque facilis soluta facere dolorem accusamus,
-      voluptas dolore recusandae eos, possimus maiores sint laudantium nobis
-      atque est esse culpa? Consequuntur, iure totam! Repellendus aliquam
-      quibusdam eligendi laudantium nemo maiores ipsum quia magni aut? Sit, ex
-      molestiae minus quis cum veniam nisi voluptatum quas fuga adipisci ut,
-      accusantium distinctio dignissimos facere laboriosam officiis! Excepturi
-      omnis dolor rerum eos, saepe nulla quam soluta, recusandae perferendis
-      incidunt eaque explicabo possimus, dolorem commodi ullam quisquam rem
-      harum veritatis eius sunt est inventore dolore! Ab, consectetur soluta.
-      Aut ut nemo cum, in quo nulla, eveniet a animi tempore quod dolore iusto
-      consectetur omnis, quis qui tenetur reiciendis alias possimus? Iste
-      commodi neque distinctio possimus impedit architecto incidunt? Cum est
-      cupiditate maxime magni quaerat omnis dolor, facilis quam velit laboriosam
-      nihil tempore iusto in, fugit perspiciatis error id! Voluptatibus
-      provident rerum alias accusantium inventore enim veniam. Mollitia, rem! Ab
-      quidem aperiam sapiente dignissimos debitis earum saepe nam nihil
-      doloribus alias modi explicabo, dolorem, cumque quis soluta, repellendus
-      voluptates incidunt aut ratione quisquam blanditiis autem molestiae
-      necessitatibus tempore! Provident?
-    </>
+    <div className="app">
+      <div className="row">
+        <div className="mixed-chart">
+          <Chart
+            options={state.options}
+            series={state.series}
+            type="area"
+            width="800"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
